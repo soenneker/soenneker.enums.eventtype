d673042f6adba0153fa7c13b0535e357f149d984
Jake Soenneker <jake@soenneker.com>
2025-02-22 13:23:33 -0600

    SmartEnum -> Intellenum

--- src/EventType.cs
+++ src/EventType.cs
@@ -1,18 +1,18 @@
-﻿using Soenneker.SmartEnum.Named;
+﻿using Intellenum;
 
 namespace Soenneker.Enums.EventType;
 
 /// <summary>
 /// A generic enum for CRUD (Create, Read, Update, Delete)
 /// </summary>
-public sealed class EventType : NamedSmartEnum<EventType>
+[Intellenum<string>]
+public partial class EventType
 {
-    public static readonly EventType Create = new(nameof(Create), 0);
-    public static readonly EventType Read = new(nameof(Read), 1);
-    public static readonly EventType Update = new(nameof(Update), 2);
-    public static readonly EventType Delete = new(nameof(Delete), 3);
+    public static readonly EventType Create = new(nameof(Create));
 
-    private EventType(string name, int value) : base(name, value)
-    {
-    }
+    public static readonly EventType Read = new(nameof(Read));
+
+    public static readonly EventType Update = new(nameof(Update));
+
+    public static readonly EventType Delete = new(nameof(Delete));
 }
